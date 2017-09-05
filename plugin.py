13066e4becff2e467497f3b23c7b0cfe40ac938b
Gerard Roche <gerardroche@users.noreply.github.com>
2017-09-05 21:16:45 +0100

    Support .tmTheme for ColorSchemeUnit

--- plugin.py
+++ plugin.py
@@ -1,5 +1,4 @@
 import os
-import re
 
 from sublime_plugin import WindowCommand
 
@@ -80,14 +79,30 @@ _COMMANDS = {
 }
 
 
+_EXTENSIONS = {
+    '.tmTheme': 'color_scheme_unit'
+}
+
+
+_NAME_STARTS = {
+    'color_scheme_test': 'color_scheme_unit'
+}
+
+
 def _get_context(window):
     view = window.active_view()
     if view:
         file_name = view.file_name()
-
         if file_name:
-            if bool(re.match('^color_scheme_test.*\.[a-zA-Z0-9]+$', os.path.basename(file_name))):
-                return 'color_scheme_unit'
+            f_path, f_base = os.path.split(file_name)
+            f_name, f_ext = os.path.splitext(f_base)
+
+            for k, v in _NAME_STARTS.items():
+                if f_name.startswith(k):
+                    return v
+
+            if f_ext in _EXTENSIONS:
+                return _EXTENSIONS[f_ext]
 
         if view.match_selector(view.sel()[0].begin(), 'source.python'):
             return 'sublimetext'
@@ -97,8 +112,21 @@ def _get_context(window):
 
 def _run_command(window, name):
     try:
-        command = _COMMANDS[name][_get_context(window)]
-        print('Test: run command \'{}\''.format(command))
+        view = window.active_view()
+        is_debug = view.settings().get('test.debug') if view else window.settings().get('test.debug')
+        if is_debug:
+            print('Test: view=[id={},file={}]'.format(view.id(), view.file_name()))
+
+        context = _get_context(window)
+
+        if is_debug:
+            print('Test: in \'{}\' context'.format(context))
+
+        command = _COMMANDS[name][context]
+
+        if is_debug:
+            print('Test: run \'{}\' command'.format(command))
+
         window.run_command(command)
     except KeyError:
         print('Test: test not found')
